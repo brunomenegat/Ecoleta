002393534bbf5c7730b9acc072ef0144ee36dcd9
V-Mihael <vmharissis@gmail.com>
2020-06-06 18:45:34 -0300

    1h 35m 21s

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const server = express()
+
+//Pasta publica
+server.use(express.static("public"))
+
+//Template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+server.get("/", (req,res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req,res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req,res) => {
+    return res.render("search-results.html")
+})
+
+server.listen(3000)
+

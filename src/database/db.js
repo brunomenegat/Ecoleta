456a3009317fc0d4e53df6cabb6e2619fc999ccc
kakaroto12 <b.menegatt@hotmail.com>
2020-06-07 15:13:23 -0300

    Ultima aula 40 min

---
+++ src/database/db.js
@@ -0,0 +1,68 @@
+const sqlite3 = require("sqlite3").verbose()
+const db = new sqlite3.Database("./src/database/database.db")
+
+
+
+//utilizar banco de dados
+db.serialize(() => {
+
+    //Criar tabela
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `)
+    //Inserir dados
+    const query = `
+INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?);
+
+`
+
+const values = [
+        "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1401&q=80",
+        "Colectoria",
+        "Guilher, Jardim",
+        "Nº 260",
+        " Santa Catarina",
+        "Rio do Sul",
+        "Resíduos Eletrônicos, Lâmpadas"
+    ]
+
+    function afterInsertData(err) {
+
+        if(err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+    }
+
+   // db.run(query, values, afterInsertData)
+
+
+    //consultar dados
+
+
+
+
+    //deletar dados
+
+})
+
+
